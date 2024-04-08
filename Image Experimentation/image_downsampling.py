599d490d3a951eec937b4d89a7c50f8ce98675b1
Ravi Raghavan <ravi0308raghavan@gmail.com>
2024-04-08 19:12:14 -0400

    Coded up a Rough Version of Algorithm #1 but Still Need to add the Gradient Descent w.r.t to the Blurring and Downsampling Matrix - Ravi Raghavan

--- Image Experimentation/image_downsampling.py
+++ Image Experimentation/image_downsampling.py
@@ -4,10 +4,19 @@ import cv2
 image = cv2.imread('test.jpeg')
 
 # Downsample the image using OpenCV's resize function
-downsampled_image = cv2.pyrDown(image)
+hIm = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+upscale = 2
+
+print(hIm.shape)
+
+lIm = cv2.resize(hIm, tuple(int(x * (1/upscale)) for x in hIm.shape)[::-1], interpolation = cv2.INTER_CUBIC)
+print(lIm.shape)
+
+lIm = cv2.resize(lIm, hIm.shape[::-1], interpolation = cv2.INTER_CUBIC)
+print(lIm.shape)
 
 # Display the downsampled image
-cv2.imshow('Original Image', image)
-cv2.imshow('Downsampled Image', downsampled_image)
+cv2.imshow('High Resolution Image', hIm)
+cv2.imshow('Low Resolution Image', lIm)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
