3d160ab0049318f8b55a43a611d7754c1817227d
Ravi Raghavan <ravi0308raghavan@gmail.com>
2024-04-10 14:41:51 -0400

    Able to reconstruct Downsampled Matrix

---
+++ Downsampling/test.py
@@ -0,0 +1,22 @@
+#Test Class to downsample images
+import numpy as np
+from scipy.sparse import csr_matrix
+
+#Downsample an Image
+def downsample(image: np.ndarray):
+    M, N = image.shape
+    downsample_matrix = np.eye(M * N)
+    
+    data = np.ones(M * N)
+    rows = np.arange(M * N)
+    
+    cols = np.repeat(np.arange(start = 0, stop = N, step = 2), 2)[np.newaxis, :] + (N * (np.arange(start = 0, stop = M, step = 2)[:, np.newaxis]))
+    cols = np.repeat(cols, 2, axis = 0) 
+    cols = cols.flatten()
+    
+    print(len(data), len(rows), len(cols))
+    downsample_matrix = csr_matrix((data, (rows, cols)), shape = (M * N, M * N))
+    return downsample_matrix
+
+
+image = np.random.randn(512, 512)
