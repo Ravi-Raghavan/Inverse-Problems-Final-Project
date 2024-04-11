ff6b02c5e48d628e4765b4756bffc54298634959
Ravi Raghavan <ravi0308raghavan@gmail.com>
2024-04-11 16:02:57 -0400

    Working on Training Joint Dictionaries

--- Dictionaries/dictionary_training.py
+++ Dictionaries/dictionary_training.py
@@ -3,9 +3,9 @@ from patch_pruning import patch_pruning
 from train_coupled_dict import train_coupled_dict
 
 ### Code used to Train Dictionaries
-training_image_path = "Data/Training"
+training_image_path = "../Data/Training"
 
-dict_size = 10 #Dictionary Size will be 512
+dict_size = 512 #Dictionary Size will be 512
 lamb = 0.15 #sparsity regularization
 patch_size = 5 #size of patches will be 5 x 5
 nSmp = 5 #number of patches to sample
@@ -16,7 +16,6 @@ Xh, Xl = rnd_smp_patch(training_image_path, '.bmp', patch_size, nSmp, upscale)
 
 #Prune patches with small variance
 Xh, Xl = patch_pruning(Xh, Xl, threshold = 10)
-print(Xh.shape, Xl.shape)
 
 #Joint Dictionary Training
 train_coupled_dict(Xh, Xl, dict_size, lamb)
